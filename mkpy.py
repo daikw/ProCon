ad476a15cc6c307fcaf5a73e24f6163b1cf17269
daik_ <hazuhi@gmail.com>
2017-05-17 13:56:03 +0900

    debug mkpy and config

--- mkpy.py
+++ mkpy.py
@@ -30,11 +30,16 @@ if __name__ == "__main__":
 	contestname = config['contestname']
 	json_handler.close()
 
-	filename = contestname + name + num.rjust(3, '0') + ''.join(map(str, args)) + '.py'
-	if os.path.exists(filename):
-		raise Exception('this file name is already exists')
+	postfix = ''
+	if contestname == 'atcoder/':
+		postfix = 'bcd'
 
-	f = open(filename, 'a')
-	f.write(text)
-	sp.Popen(['open', filename])
-	f.close()
+	for letter in postfix:
+		filename = contestname + name + num.rjust(3, '0') + ''.join(map(str, args)) + letter + '.py'
+		if os.path.exists(filename):
+			raise Exception('this file name is already exists')
+
+		f = open(filename, 'a')
+		f.write(text)
+		sp.Popen(['open', filename])
+		f.close()
