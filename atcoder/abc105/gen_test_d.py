578ce0b3f6d41b0e4e9b9ebe4b3e26fffd5cdbca
Daiki Watanabe <hazuhi@gmail.com>
2018-08-16 22:38:57 +0900

    add test case generator

---
+++ atcoder/abc105/gen_test_d.py
@@ -0,0 +1,22 @@
+from d import count_lr
+import random
+import pickle
+
+
+def generate_test_case(size, number_size):
+    test_cases = []
+    for _ in range(size):
+        n = random.randint(1, number_size)
+        m = random.randint(2, number_size)
+        a = [random.randint(1, number_size) for _ in range(10**4)]
+
+        count = count_lr(n, m, a)
+
+        test_cases.append(((n, m), a, count))
+
+    with open('test_case_d.pickle', mode='wb') as f:
+        pickle.dump(test_cases, f)
+
+
+if __name__ == "__main__":
+    generate_test_case(3, 10**3)
