197b73ff9b049e377f274727c6c69f8d2d949bc3
Daiki Watanabe <hazuhi@gmail.com>
2018-08-16 22:40:03 +0900

    Refactor: avoid variable name conflict

--- atcoder/abc105/test_d.py
+++ atcoder/abc105/test_d.py
@@ -10,14 +10,14 @@ class Test(unittest.TestCase):
     def tearDown(self):
         print('after test')
 
-    test_cases = [
+    sample = [
         ((3, 2), (4, 1, 5), 3),
         ((13, 17), (29, 7, 5, 7, 9, 51, 7, 13, 8, 55, 42, 9, 81), 6),
         ((10, 400000000), (1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000, 1000000000), 25),
     ]
 
-    def test_result(self):
-        for test_case in self.test_cases:
+    def test_sample(self):
+        for test_case in self.sample:
             n, m = test_case[0]
             a = test_case[1]
             eq_(count_lr(n, m, a), test_case[2])
