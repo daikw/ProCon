63a19215d7e9f3616e577a08a394fc0eab8fc206
Daiki Watanabe <hazuhi@gmail.com>
2018-08-18 20:42:12 +0900

    Update template

--- _templates/template_lite.py
+++ _templates/template_lite.py
@@ -3,12 +3,10 @@ import itertools as it
 import collections as cl
 import functools as ft
 
-
-def LI(): return [int(x) for x in sys.stdin.readline().split()]
-def LI_(): return [int(x)-1 for x in sys.stdin.readline().split()]
-def LF(): return [float(x) for x in sys.stdin.readline().split()]
-def LS(): return sys.stdin.readline().split()
-def I(): return int(sys.stdin.readline())
-def F(): return float(sys.stdin.readline())
+def LI(): return list(map(int, input().split()))
+def LI_(): return list(map(lambda x: int(x) - 1, input().split()))
+def LF(): return list(map(float, input().split()))
+def LS(): return input().split()
+def I(): return int(input())
+def F(): return float(input())
 def S(): return input()
-
