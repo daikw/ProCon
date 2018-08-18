1c61b7caf81ba2e87ab0904b5f14e49db85d2bbc
Daiki Watanabe <hazuhi@gmail.com>
2018-08-18 11:14:03 +0900

    Improve count_lr: according to the coset of M
    and change the name of the old one

--- atcoder/abc105/d.py
+++ atcoder/abc105/d.py
@@ -13,12 +13,34 @@ def F(): return float(sys.stdin.readline())
 def S(): return input()
 
 
-def count_lr(n, m, a):
+def count_lr_old(n, m, a):
     count = 0
     for l in range(n):
         for r in range(l, n):
             if not sum(a[l:r+1]) % m:
                 count += 1
+    return count
+
+
+def count_lr(n, m, a):
+    b_acm = 0
+    mod_m_counts = {}
+    for ai in a:
+        b_acm = (b_acm + ai) % m
+        mod_m_counts.setdefault(b_acm, 0)
+        mod_m_counts[b_acm] += 1
+
+    print(mod_m_counts)
+
+    count = 0
+    all_ = 0
+    for k, v in mod_m_counts.items():
+        all_ += v
+        count += v * (v - 1) // 2
+        if k == 0:
+            count += v
+
+    print('all_', all_)
     return count
 
 
--- atcoder/abc105/gen_test_d.py
+++ atcoder/abc105/gen_test_d.py
@@ -1,4 +1,4 @@
-from d import count_lr
+from d import count_lr_old
 import random
 import pickle
 
@@ -10,7 +10,7 @@ def generate_test_case(size, number_size):
         m = random.randint(2, number_size)
         a = [random.randint(1, number_size) for _ in range(10**4)]
 
-        count = count_lr(n, m, a)
+        count = count_lr_old(n, m, a)
 
         test_cases.append(((n, m), a, count))
 
