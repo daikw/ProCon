9fb8a25cc9a03f02377954b59fb8a3d197084f80
Daiki Watanabe <hazuhi@gmail.com>
2018-10-07 20:02:26 +0900

    Feat: turn the math functions into a new class

--- _templates_py/tools.py
+++ _templates_py/tools.py
@@ -1,26 +1,27 @@
-def gcd(a, b):
-    if a < b:
-        return gcd(b, a)
-    if b == 0:
+class FutureMath:
+    def gcd(self, a, b):
+        if a < b:
+            return self.gcd(b, a)
+        if b == 0:
+            return a
+        while b:
+            a, b = b, a % b
         return a
-    while b:
-        a, b = b, a % b
-    return a
+
+    def gcd_(self, list):
+        result = list[0]
+        for el in list[1:]:
+            result = self.gcd(result, el)
+        return result
+
+    def lcm(self, a, b):
+        return a * b // self.gcd(a, b)
+
+    def lcm_(self, list):
+        result = list[0]
+        for el in list[1:]:
+            result = self.lcm(result, el)
+        return result
 
 
-def gcd_(list):
-    result = list[0]
-    for el in list[1:]:
-        result = gcd(result, el)
-    return result
-
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-
-def lcm_(list):
-    result = list[0]
-    for el in list[1:]:
-        result = lcm(result, el)
-    return result
+future_math = FutureMath()
