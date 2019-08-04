21f321a564631f910f19eb54a9915acfd89ebf23
Daiki Watanabe <hazuhi@gmail.com>
2019-08-04 13:39:17 +0900

    :recycle: remove unnecessary conditioning

--- atcoder/dp/c3.py
+++ atcoder/dp/c3.py
@@ -1,13 +1,12 @@
 n = int(input())
 a, b, c = list(map(int, input().split()))
 
-if n > 1:
-    for _ in range(n-1):
-        a_, b_, c_ = list(map(int, input().split()))
-        a_ += max(b, c)
-        b_ += max(a, c)
-        c_ += max(a, b)
-        a, b, c = a_, b_, c_
+for _ in range(n-1):
+    a_, b_, c_ = list(map(int, input().split()))
+    a_ += max(b, c)
+    b_ += max(a, c)
+    c_ += max(a, b)
+    a, b, c = a_, b_, c_
 
 print(max(a, b, c))
 """
